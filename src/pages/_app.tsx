3e9fd9d1a9c743af5c8b271356d8cfb421b362a2
coltran3 <luca.santos>
2021-05-04 20:17:55 -0400

    feat: add Topbar to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,28 +7,26 @@ import { Container as MuiContainer } from '@material-ui/core'
 const Container = styled(MuiContainer)`
   height: 100vh;
 `
+import { TopBar } from '../components/TopBar'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Head>
         <title>Fast Form</title>
-
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
-
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Container>
+          <TopBar />
+
           <Component {...pageProps} />
         </Container>
       </ThemeProvider>
