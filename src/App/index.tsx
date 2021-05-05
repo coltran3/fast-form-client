8a5aa1ae530bc627edb434da7e7a8b86e3a77309
coltran3 <luca.santos>
2021-05-04 23:13:30 -0400

    chore: add global styles

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,9 +3,31 @@ import { Routes } from "./Routes";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  html, 
   body {
-    color:  black;
-    height: 100vh;
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background:#fff;
+    width: 100%;
+    height: 100%;
+    scroll-behavior: smooth;
+    overflow-x: hidden;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  #root {
+    height: 100%;
+    width: 100%;
   }
 `;
 
