4ed6936f6bb48a46dedb8dc22547e0f3a9d07365
coltran3 <luca.santos>
2021-05-10 22:54:00 -0400

    feat: add container to app

--- src/App/Routes/LoggedRoutes.tsx
+++ src/App/Routes/LoggedRoutes.tsx
@@ -2,10 +2,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
 import { TopBar } from "../../components/Topbar";
 import { useAuthContext } from "../../stores";
+import { Container as MuiContainer } from "@material-ui/core";
+import styled from "styled-components";
 
 const Home = React.lazy(async () => import("../../pages/Home").then(m => ({ default: m.Home })));
 const Exams = React.lazy(async () => import("../../pages/Exams").then(m => ({ default: m.Exams })));
 
+const Container = styled(MuiContainer)`
+  height: 100%;
+  padding-top: 64px !important;
+  overflow: auto;
+`;
 
 export function LoggedRoutes() {
   const { setUser } = useAuthContext();
@@ -13,10 +20,12 @@ export function LoggedRoutes() {
   return (
     <Router>
       <TopBar logout={() => setUser("")} />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/exams" component={Exams} />
-      </Switch>
+      <Container>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/exams" component={Exams} />
+        </Switch>
+      </Container>
     </Router>
   );
 }
