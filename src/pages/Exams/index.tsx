730ca7e61cf45d38c1208921c06a88a90659c843
Danilo Nascimento <danilo.nascimento@mesainc.com.br>
2021-05-11 21:24:04 -0300

    feat(component): create component home

--- src/pages/Exams/index.tsx
+++ src/pages/Exams/index.tsx
@@ -1,107 +1,35 @@
 import React from "react";
+import { Input } from "@material-ui/core";
 import Card from "../../components/card";
 import GroupDisplaying from "../../components/group-displaying";
 
 import { Container, ContentCard } from "./styles";
 
 export function Exams() {
+  const mockCard = [
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+    <Card title=" Lorem ipsum" status="aberto" />,
+  ];
   return (
     <Container>
-      <GroupDisplaying
-        title="Avalições recentes"
-        children={
-          <ContentCard>
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-          </ContentCard>
-        }
-      />
-      <GroupDisplaying
-        title="Novo"
-        children={
-          <ContentCard>
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-          </ContentCard>
-        }
-      />
-      <GroupDisplaying
-        title="Aberto"
-        children={
-          <>
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-          </>
-        }
-      />
-      <GroupDisplaying
-        title="Não disponível"
-        children={
-          <>
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-          </>
-        }
-      />
-      <GroupDisplaying
-        title="Concluído"
-        children={
-          <>
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-            <Card title=" Lorem ipsum" status="aberto" />
-          </>
-        }
-      />
+      <div className="content-input">
+        <Input placeholder="buscar avaliação" />
+      </div>
+      <GroupDisplaying title="Avalições recentes" children={<ContentCard>{mockCard.map(e => e)}</ContentCard>} />
+      <GroupDisplaying title="Novo" children={<ContentCard>{mockCard.map(e => e)}</ContentCard>} />
+      <GroupDisplaying title="Aberto" children={<>{mockCard.map(e => e)}</>} />
+      <GroupDisplaying title="Não disponível" children={<>{mockCard.map(e => e)}</>} />
+      <GroupDisplaying title="Concluído" children={<>{mockCard.map(e => e)}</>} />
     </Container>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,13 @@
+import React from "react";
+import { StartedExam } from "../../components/startedExam";
+import { Container } from "./styles";
+
 export function Home() {
-  return <div>me mama freire</div>;
+  return (
+    <Container>
+      <StartedExam title="Criar Avaliação" description=" descrição" />
+      <div className="divider"></div>
+      <StartedExam title="Criar Grupo" description=" descrição" />
+    </Container>
+  );
 }
