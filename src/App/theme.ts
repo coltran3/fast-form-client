04c0773e94a77d97f3ae85f5c5232848238cc48e
coltran3 <luca.santos>
2021-05-12 22:35:06 -0400

    feat: add more colors to app theme

--- src/App/theme.ts
+++ src/App/theme.ts
@@ -1,3 +1,5 @@
 export const theme = {
-  "primary-color": "blue",
+  "primary-color": { default: "#3f51b5", light: "#757de8", dark: "#002984" },
+  "secondary-color": { default: "#f44336", light: "#ff7961", dark: "#ba000d" },
+  grey: { "2": "#212121", "4": "#484848" },
 };
