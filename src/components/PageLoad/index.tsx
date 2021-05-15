8956c863ebcf751cb79ab78d22d2cfeefa6b1bd8
coltran3 <luca.santos>
2021-05-15 18:29:24 -0400

    feat: create CircularProgress component

---
+++ src/components/PageLoad/index.tsx
@@ -0,0 +1,7 @@
+import { CircularProgress as MuiCircularProgress } from "@material-ui/core";
+import styled from "styled-components";
+
+export const PageLoad = styled(MuiCircularProgress)`
+  margin: 0 auto;
+  display: block;
+`;
