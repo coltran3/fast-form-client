555f12be5b810f31f418c0cf4562376b30aa872a
coltran3 <luca.santos>
2021-05-15 20:53:53 -0400

    fix: display user in user main page

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,6 +1,6 @@
 import { Switch, Route, useHistory, useRouteMatch } from "react-router-dom";
 import { lazy } from "react";
-import { Button } from "@material-ui/core";
+import { Button, List, ListItem, ListItemText } from "@material-ui/core";
 import styled from "styled-components";
 import { apiClient } from "../../api";
 import { useQuery } from "react-query";
@@ -8,6 +8,9 @@ import { User } from "./types";
 import { useAuthContext } from "../../stores";
 import { ErrorView } from "../../components/ErrorView";
 import { PageLoad } from "../../components/PageLoad";
+import { ApiEntityWrapper } from "../../api/types";
+import { USER_ROLE_TRANSLATE_OBJ, USER_TYPE_TRANSLATE_OBJ } from "./constants";
+import dayjs from "dayjs";
 import { PagesTitle } from "../../components/PagesTitle";
 
 const Create = lazy(async () => import("./Create").then(m => ({ default: m.Create })));
@@ -17,35 +20,68 @@ const MainWrapper = styled.div`
   flex-direction: column;
 `;
 
-const CreateButton = styled(Button)`
-  align-self: flex-end;
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 function Main() {
   const { push } = useHistory();
   const { url } = useRouteMatch();
   const { user: token } = useAuthContext();
-  const { data, isLoading, isError } = useQuery<any>("users", () => {
-    return (apiClient.get("/user", { headers: { Authorization: `Bearer ${token}` } }) as unknown) as User;
-  });
+  const { data: user, isLoading, isError } = useQuery<ApiEntityWrapper<User>>(
+    "users",
+    () => {
+      return apiClient.get("/user", { headers: { Authorization: `Bearer ${token}` } });
+    },
+    {
+      onSuccess(user) {
+        console.log("user", user);
+      },
+    },
+  );
 
   function handleCreateClick() {
     push(`${url}/create`);
   }
 
-  if (isLoading) {
-    return <PageLoad />;
-  }
-
   return (
     <MainWrapper>
-      <CreateButton variant="contained" color="primary" onClick={handleCreateClick}>
-        Cadastrar usuarios
-      </CreateButton>
-      {isError ? (
+      <Header>
+        <PagesTitle>Usuário</PagesTitle>
+        <Button variant="contained" color="primary" onClick={handleCreateClick}>
+          Cadastrar usuários
+        </Button>
+      </Header>
+      {isLoading ? (
+        <PageLoad />
+      ) : isError ? (
         <ErrorView>Houve algum erro na busca de usuários</ErrorView>
-      ) : data && data.data && data.data.data ? (
-        <PagesTitle>{data.data.data.cpf}</PagesTitle>
+      ) : user && user.data && user.data.data ? (
+        <List>
+          <ListItem>
+            <ListItemText>
+              CPF: {user.data.data.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, `***.\$2.\$3-***`)}
+            </ListItemText>
+          </ListItem>
+
+          <ListItem>
+            <ListItemText>Matrícula: {user.data.data.enrollment}</ListItemText>
+          </ListItem>
+
+          <ListItem>
+            <ListItemText>Tipo: {USER_TYPE_TRANSLATE_OBJ[user.data.data.type]}</ListItemText>
+          </ListItem>
+
+          <ListItem>
+            <ListItemText>Função: {USER_ROLE_TRANSLATE_OBJ[user.data.data.role]}</ListItemText>
+          </ListItem>
+
+          <ListItem>
+            <ListItemText>Data de criação: {dayjs(user.data.data.createdAt).format("DD/MM/YYYY")}</ListItemText>
+          </ListItem>
+        </List>
       ) : null}
     </MainWrapper>
   );
