2294671e1f1b34b066dd482fa07119d539b5ef1b
coltran3 <luca.santos>
2021-05-16 08:17:52 -0400

    feat: add Question typing

--- src/pages/Exams/types.ts
+++ src/pages/Exams/types.ts
@@ -12,3 +12,17 @@ export interface ExamsState {
   exam?: Exam;
   setExam: Dispatch<SetStateAction<Exam | undefined>>;
 }
+
+export interface QuestionGroup {
+  title: string;
+  type: "noType" | "class" | "personal";
+  questions: Question[];
+}
+
+export interface Question {
+  required: boolean;
+  statement: string;
+  imageUrl: string;
+  imageAlt: string;
+  groupId: 0;
+}
