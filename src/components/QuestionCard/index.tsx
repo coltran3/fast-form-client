009dcb7281fedc9b719ab9639aa8b9386c0d7aec
coltran3 <luca.santos>
2021-05-19 22:14:54 -0400

    feat: add obrigatory mark to question

--- src/components/QuestionCard/index.tsx
+++ src/components/QuestionCard/index.tsx
@@ -3,13 +3,14 @@ import {
   DraggableProvidedDraggableProps,
   DraggableProvidedDragHandleProps,
 } from "react-beautiful-dnd";
-import { Card, CardActions, CardContent, IconButton } from "@material-ui/core";
+import { Card, CardActions, CardContent, IconButton, Tooltip } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
 import { FlexSpacer } from "../FlexSpacer";
 import DeleteIcon from "@material-ui/icons/Delete";
 import ImageIcon from "@material-ui/icons/Image";
 import EditIcon from "@material-ui/icons/Edit";
+import SmsFailedRoundedIcon from "@material-ui/icons/SmsFailedRounded";
 
 interface Props {
   idx: number;
@@ -27,6 +28,15 @@ const StyledCard = styled(Card)`
   align-items: center;
 `;
 
+const StyledCardContent = styled(CardContent)`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledSmsFailedRoundedIcon = styled(SmsFailedRoundedIcon)`
+  margin-left: 1rem;
+`;
+
 export function QuestionCard({
   idx,
   title,
@@ -39,14 +49,18 @@ export function QuestionCard({
 }: Props) {
   return (
     <StyledCard ref={provided.innerRef} {...draggableProps} {...dragHandleProps} className="card">
-      <CardContent>
+      <StyledCardContent>
         <Typography variant="h6" component="h2" color="primary">
           {idx} - {title}
         </Typography>
-      </CardContent>
+
+        <Tooltip title="Essa pergunta é obrigatória">
+          <StyledSmsFailedRoundedIcon type="filled" color="primary" />
+        </Tooltip>
+      </StyledCardContent>
       <FlexSpacer />
       <CardActions>
-        <IconButton aria-label="edit" size="small" onClick={onClickEdit}>
+        <IconButton aria-label="edit" size="small" color="primary" onClick={onClickEdit}>
           <EditIcon />
         </IconButton>
 
